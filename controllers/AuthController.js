2abbf44edf394e66c65f5f147e6ec1137f5a5233
msiroilem <twjones86@gmail.com>
2021-11-11 19:22:59 -0500

    added AuthController, AuthRouter and middleware/index.js

---
+++ controllers/AuthController.js
@@ -0,0 +1,73 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.body.username },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.password_digest,
+        req.body.password
+      ))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    let password_digest = await middleware.hashPassword(password)
+    const user = await User.create({ username, email, password_digest })
+    res.send({ username: user.username, email: user.email })
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+const UpdatePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body
+    const user = await User.findByPk(req.params.user_id)
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.dataValues.passwordDigest,
+        oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(newPassword)
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Ok', payload: user })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+module.exports = {
+  Login,
+  Register,
+  UpdatePassword,
+  CheckSession
+}
